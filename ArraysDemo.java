d4f2682f75af9ebfce623c10c6769942bf2c39ec
denoc1 <176614645+denoc1@users.noreply.github.com>
2025-11-14 02:31:30 +0000

    template

---
+++ ArraysDemo.java
@@ -0,0 +1,35 @@
+public class ArrayDemo{
+
+  public static void main(String[] args){
+      //declare an array that can hold the name of 15 people
+
+
+      //declare an array that can hold the gpas for each of the 15 people
+
+      //declare an array that contains the scores 10, 15, -8 and 14
+
+      
+
+      //print the first value in the name array
+
+      //assign the first value of the name array to "Frank"
+
+      //assign the first value in the gpa array to 3.8
+
+      //Print the first values in the name and gpa array in the format:  Frank has a gpa of 3.8  (Be sure to reference the array values)
+
+
+
+
+      //add up the values in the scores array and store the value in a new variable called sum
+
+      
+      //Print the length of the name array and the scores array
+
+
+      //Assign the last value of the names array to "Alexis"
+      //Assign the last value of the gpa array to 4.0
+
+
+  }//end main
+} //end class
